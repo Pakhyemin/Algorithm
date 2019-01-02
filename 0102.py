e544cf7a440d045e167f1cd6b63a1a73007c9279
Pakhyemin <hm951201@naver.com>
2019-01-02 23:19:31 +0900

    9498

---
+++ 0102.py
@@ -0,0 +1,12 @@
+grade=int(input("성적을 입력하세요: "))
+if grade>=90 and grade<100:
+    print("A")
+elif grade>=80 and grade<90:
+    print("B")
+elif grade>=70 and grade<80:
+    print("C")
+elif grade>=60 and grade<70:
+    print("D")
+else:
+    print("F")
+
